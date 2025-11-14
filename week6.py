ab5e4a5171084edfc7260e5cb3c3a1e3200ed51d
SP Ragav Balaji <ragavbalajisp@gmail.com>
2025-11-14 17:17:01 +0100

    Week 6 File

---
+++ week6.py
@@ -0,0 +1,32 @@
+#animals=["dog","cat","rabbit","hamster","parrot"]
+#print(animals)
+#print(animals[0],animals[-1])
+#animals[4]="fish"
+#print(animals)
+#animals.append("turtle")
+#animals.insert(3,"pig")
+#print(animals)
+#animals.pop(3)
+#print(animals)
+#index=[]
+#for animal in animals:
+#    print(animal,animals.index(animal))
+#print(animals[1:5])
+
+#numbers=[1,2,3]
+#sum=0
+#for i in numbers:
+#    sum=sum+i
+#print("the sum is:", sum)
+#
+#for i in numbers:
+#    lnum=[0]
+#    if i>lnum[0]:
+#        lnum.pop()
+#        lnum.append(i)
+#print("the largest number is:", lnum)
+
+d={"name""a":"ragav","age":20,"city":"chennai"}
+print(d)
+
+    
